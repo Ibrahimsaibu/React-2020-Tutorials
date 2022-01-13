84d631f002d94da73fea578d946dbf42c457ef7e
Ibrahim Saibu <ibrahimsaibu2010@gmail.com>
2022-01-13 05:50:40 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from "react";
+
+import UseEffectBasics from "./tutorial/2-useEffect/setup/1-useEffect-basics";
+
 function App() {
   return (
-    <div className='container'>
-      <h2>Advanced Tutorial</h2>
+    <div className="container">
+      <UseEffectBasics />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState('hello world'));
+  // const value = useState(1)[0]
+  // const handler = useState(1)[1]
+  // console.log(value, handler);
+  const [text, setText] = useState("randle text");
+
+  const handleClick = () => {
+    if (text === "randle title") {
+      setText("hello world");
+    } else {
+      setText("randle title");
+    }
+  };
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>
+        Change Text
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
